027b6e4b7666fcd9c8656ca6491a4966200bc7fb
Otlhomame <43067037+Otlhomame@users.noreply.github.com>
2022-07-11 00:37:42 +0100

    Delete FAQ Collapse directory

--- FAQ Collapse/script.js
+++
@@ -1,9 +0,0 @@
-//Bringing in all the toggle buttons
-const toggles = document.querySelectorAll('.faq-toggle')
-
-//Lop through each toggle button and listen for a click
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active') //toggling the class on teh parent node which is the div that has class "faq active"
-    })
-})
